60b81545e2585f781eb163f1339eeb5f272d10c2
root <root@ip-172-31-14-159.ap-south-1.compute.internal>
2026-06-12 08:34:08 +0000

    modified app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hi from DevOps CI/CD Pipeline!"
+    return "Hi from DevOps CI/CD Pipeline hello!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
